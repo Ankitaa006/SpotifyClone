9e3640cf662ae0439c3213d39f0c061b70b00474
TheFrustrator <sudip.bhunia23@tnu.in>
2026-06-22 15:03:11 +0530

    complete play, next, previous, SeekBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import SideBar from './components/SideBar'
-import Player from './components/Player'
-import Display from './components/Display'
+import React, { useContext } from "react";
+import SideBar from "./components/SideBar";
+import Player from "./components/Player";
+import Display from "./components/Display";
+import { PlayerContext } from "./context/Playercontext";
 
 const App = () => {
+  const { audioRef, track } = useContext(PlayerContext);
   return (
-    <div className='h-screen bg-black'>
-      <div className='h-[90%] flex'>
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex">
         <SideBar />
         <Display />
       </div>
       <Player />
+      <audio ref={audioRef} src={track.file} preload="auto"></audio>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
